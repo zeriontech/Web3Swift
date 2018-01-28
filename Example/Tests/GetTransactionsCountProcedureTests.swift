0e07c564185263765f17024f1a3ebf5194618ecb
Timofey Solonin <tim-solonin2@yandex.ru>
2018-01-28 17:20:51 +0300

    Added Quick/Nimble. Updated existing tests to use new matchers frameworks.

--- Example/Tests/GetTransactionsCountProcedureTests.swift
+++ Example/Tests/GetTransactionsCountProcedureTests.swift
@@ -1,11 +1,14 @@
 import XCTest
+import Quick
+import Nimble
 @testable import Web3Swift
 
 //TODO: We really need to think about the structure of out unit tests and how we can pass dependencies (such as infura url and address) to the tests themselves
 final class GetTransactionsCountProcedureTests: XCTestCase {
     
     func testNotEmptyData() {
-        let data = try? GetTransactionsCountProcedure(
+        expect{
+            try GetTransactionsCountProcedure(
                 network: SimpleNetwork(
                     session: URLSession.shared,
                     url: URL(string: "https://mainnet.infura.io/3O4Ywm6wGFgpIn8G10TT")!,
@@ -14,9 +17,9 @@ final class GetTransactionsCountProcedureTests: XCTestCase {
                 address: SimpleAddress(value: "0xe35d276812001e33e3A8f6f445e2D1e90ff86F1C"),
                 blockChainState: LatestBlockChainState()
             ).call()
-        
-        XCTAssertNotNil(data)
-        XCTAssert(data?.isEmpty == false)
+        }.toNot(
+            beEmpty()
+        )
     }
     
 }
