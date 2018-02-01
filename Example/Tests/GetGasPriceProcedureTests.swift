9be7917c4c48d83fd183f3eca19ece6e5b0c0493
Vadim Koleoshkin <v.koleoshkin@gmail.com>
2018-02-01 15:37:45 +0300

    Improve test

--- Example/Tests/GetGasPriceProcedureTests.swift
+++ Example/Tests/GetGasPriceProcedureTests.swift
@@ -20,41 +20,20 @@ final class GetGasPriceProcedureTests: XCTestCase {
     }
     
     func testValueGreaterThanZero() {
-        
-        do {
-            let gasPriceResponse = try GetGasPriceProcedure(
-                network: SimpleNetwork(
-                    session: URLSession.shared,
-                    url: URL(string: "https://mainnet.infura.io/3O4Ywm6wGFgpIn8G10TT")!,
-                    headers: [:]
+        expect(
+                UInt64(
+                    try GetGasPriceProcedure(
+                        network: SimpleNetwork(
+                            session: URLSession.shared,
+                            url: URL(string: "https://mainnet.infura.io/3O4Ywm6wGFgpIn8G10TT")!,
+                            headers: [:]
+                        )
+                    ).call()["result"].string().removingHexPrefix(),
+                    radix: 16
                 )
-                ).call()
-            
-            if let gasPriceString = gasPriceResponse["result"].string {
-                
-                print(gasPriceString.removingHexPrefix())
-                
-                if let gasPriceInt = UInt64(gasPriceString.removingHexPrefix(), radix: 16) {
-                    
-                    print(gasPriceInt)
-                    XCTAssertTrue(gasPriceInt > UInt64(0))
-                    
-                } else {
-                    print("Failed to parse int")
-                    XCTAssertTrue(false)
-                }
-                
-            } else {
-                
-                print("Incorrect response from JSON RPC")
-                XCTAssertTrue(false)
-                
-            }
-        } catch (let error) {
-            print(error)
-            XCTAssertTrue(false)
-        }
-        
+            ).to(
+                beGreaterThan(0)
+            )
     }
     
 }
