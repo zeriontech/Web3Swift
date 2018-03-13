42a21fc18c9f2e905386d0d7d0182e8d8419d488
Timofey Solonin <tim-solonin2@yandex.ru>
2018-03-13 16:29:20 +0300

    #81 - fix

--- Example/Tests/BytesScalar/BytesFromHexStringTests.swift
+++ Example/Tests/BytesScalar/BytesFromHexStringTests.swift
@@ -59,31 +59,29 @@ final class BytesFromHexStringTests: XCTestCase {
     }
 
     func testNonAmbiguousUnprefixedHexStrings() {
-        func testNonAmbiguousUnprefixedHexStrings() {
-            Array<(String, [UInt8])>(
-                [
-                    ("", []),
-                    ("00", [0x00]),
-                    ("01", [0x01]),
-                    ("03ff", [0x03, 0xff]),
-                    ("cD8aC90d9cc7e4c03430d58d2f3e87Dae70b807e", [0xcd, 0x8a, 0xC9, 0x0d, 0x9c, 0xc7, 0xe4, 0xc0, 0x34, 0x30, 0xd5, 0x8d, 0x2f, 0x3e, 0x87, 0xDa, 0xe7, 0x0b, 0x80, 0x7e])
-                ]
-            ).forEach{ hex, bytes in
-                expect{
-                    try BytesFromCompactHexString(
-                        hex: SimpleString(
-                            string: hex
-                        )
-                    ).value()
-                }.to(
-                    equal(
-                        Data(
-                            bytes: bytes
-                        )
-                    ),
-                    description: "Non ambiguous unprefixed hex string is expected to produce valid bytes array"
-                )
-            }
+        Array<(String, [UInt8])>(
+            [
+                ("", []),
+                ("00", [0x00]),
+                ("01", [0x01]),
+                ("03ff", [0x03, 0xff]),
+                ("cD8aC90d9cc7e4c03430d58d2f3e87Dae70b807e", [0xcd, 0x8a, 0xC9, 0x0d, 0x9c, 0xc7, 0xe4, 0xc0, 0x34, 0x30, 0xd5, 0x8d, 0x2f, 0x3e, 0x87, 0xDa, 0xe7, 0x0b, 0x80, 0x7e])
+            ]
+        ).forEach{ hex, bytes in
+            expect{
+                try BytesFromCompactHexString(
+                    hex: SimpleString(
+                        string: hex
+                    )
+                ).value()
+            }.to(
+                equal(
+                    Data(
+                        bytes: bytes
+                    )
+                ),
+                description: "Non ambiguous unprefixed hex string is expected to produce valid bytes array"
+            )
         }
     }
 
