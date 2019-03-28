df4c87c8fe3de42caf5b9fac58ae424b5a041999
Vadim Koleoshkin <vadim@koleoshkin.com>
2019-03-27 19:37:58 -0700

    add alchemyapi.io to library

---
+++ Example/Networks/MainnetAlchemyNetwork.swift
@@ -0,0 +1,44 @@
+//
+// This source file is part of the Web3Swift.io open source project
+// Copyright 2019 The Web3Swift Authors
+// Licensed under Apache License v2.0
+//
+// MainnetAlchemyNetwork.swift
+//
+// Created by Vadim Koleoshkin on 27/03/2019
+//
+
+import Foundation
+import Web3Swift
+
+//Ethereum mainnet via alchemyapi.io service and a Web3Swift.io api key
+public final class MainnetAlchemyNetwork: Network {
+    
+    private let origin: Network = AlchemyNetwork(chain: "mainnet", apiKey: "ETi2ntZoWxd6nTI1qE13Q4I1eLB8AMDl")
+    
+    /**
+     - returns:
+     Id of a mainnet (should be 1)
+     
+     - throws:
+     `DescribedError` if something went wrong
+     */
+    public func id() throws -> IntegerScalar {
+        return try origin.id()
+    }
+    
+    /**
+     - returns:
+     `Data` for a JSON RPC call to mainnet
+     
+     - throws:
+     `DescribedError` if something went wrong
+     */
+    public func call(method: String, params: Array<EthParameter>) throws -> Data {
+        return try origin.call(
+            method: method,
+            params: params
+        )
+    }
+    
+}
