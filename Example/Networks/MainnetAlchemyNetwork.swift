3c9c9f8674e57ec2521c23614e4ba5c0abc4f07f
Vadim Koleoshkin <vadim@koleoshkin.com>
2019-03-29 12:09:14 -0700

    add a note about alchemyapi

--- Example/Networks/MainnetAlchemyNetwork.swift
+++ Example/Networks/MainnetAlchemyNetwork.swift
@@ -11,7 +11,9 @@
 import Foundation
 import Web3Swift
 
-//Ethereum mainnet via alchemyapi.io service and a Web3Swift.io api key
+// Ethereum mainnet via alchemyapi.io service and a Web3Swift.io api key
+// This is a demo key provided by Alchemy for testing purposes only and has lower performance.
+// Sign up for an official key at https://alchemyapi.io if you plan to use this in any development, staging, or production environments.
 public final class MainnetAlchemyNetwork: Network {
     
     private let origin: Network = AlchemyNetwork(chain: "mainnet", apiKey: "ETi2ntZoWxd6nTI1qE13Q4I1eLB8AMDl")
