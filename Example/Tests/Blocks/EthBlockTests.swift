431bf60739eded6ec9df8432054bca8a13787752
Vadim Koleoshkin <vadim@koleoshkin.com>
2019-08-18 20:52:59 +0200

    #216 - Add block tests

--- Example/Tests/Blocks/EthBlockTests.swift
+++ Example/Tests/Blocks/EthBlockTests.swift
@@ -44,5 +44,50 @@ class EthBlockTests: XCTestCase {
             description: "Expect block number to have correct value"
         )
     }
+
+    func testBlockTimestamp()
+    {
+        expect{
+            try EthBlockHash(
+                bytes: BytesFromHexString(
+                    hex: "0x72946042d329c02d26af0a5fe7f5ec43a3e07cd243b444114f83d411376e929d"
+                ),
+                network: MainnetAlchemyNetwork()
+            ).block().timestamp().value().toDecimalString()
+        }.to(
+            equal("1566128500"),
+            description: "Expect block timestamp to have correct value"
+        )
+    }
     
+    func testBlockParentHash()
+    {
+        expect{
+            try EthBlockHash(
+                bytes: BytesFromHexString(
+                    hex: "0x72946042d329c02d26af0a5fe7f5ec43a3e07cd243b444114f83d411376e929d"
+                ),
+                network: MainnetAlchemyNetwork()
+            ).block().parentHash().value().toPrefixedHexString()
+        }.to(
+            equal("0x4228a7923f72b8c4faf7dd25c0d102ee28ad2ca9435d223046964d98e183823f"),
+            description: "Expect block parent hash to have correct value"
+        )
+    }
+    
+    func testBlockTransactions()
+    {
+        expect{
+            try EthBlockHash(
+                bytes: BytesFromHexString(
+                    hex: "0x72946042d329c02d26af0a5fe7f5ec43a3e07cd243b444114f83d411376e929d"
+                ),
+                network: MainnetAlchemyNetwork()
+            ).block().transactions().value().count
+        }.to(
+            equal(82),
+            description: "Expect block trasactions count to have correct value"
+        )
+    }
+
 }
